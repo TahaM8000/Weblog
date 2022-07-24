5376618ab03d9eb6be97e9dab0a51e93868757cb
Taha <TahaM8000@gmail.com>
2022-07-24 19:46:38 +0430

    Author page and post detail page

--- posts/views.py
+++ posts/views.py
@@ -13,4 +13,27 @@ class PostListView(ListView):
     context_object_name = "Posts"
     template_name = "posts/classBaseViews/home.html"
     queryset = models.Post.objects.published()[::-1]
-    paginate_by = 4
+    paginate_by = 2
+
+
+class PostDetail(DetailView):
+    template_name = "posts/classBaseViews/Post_detail.html"
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(models.Post.objects.published(), slug=slug)
+
+
+class AuthorDetail(ListView):
+    paginate_by = 5
+    template_name = "oldview/classview/Author_detail.html"
+
+    def get_queryset(self):
+        global Author
+        slug = self.kwargs.get('slug')
+        Author = User.objects.get(username=slug)
+        return Author.posts.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Author'] = Author
+        return context
